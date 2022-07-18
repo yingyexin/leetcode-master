79144c56de79ea86c70af6ac1ca488f68f1a740f
Yingye Xin <yingye.xin@driveblocks.ai>
2022-07-19 01:05:53 +0200

    share progress

---
+++ code/stackqueue.cpp
@@ -0,0 +1,50 @@
+#include <stack>
+#include <queue>
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <string>
+
+using namespace std;
+
+// 232E
+class MyQueue {
+public:
+    MyQueue() {
+
+    }
+    
+    void push(int x) {
+
+    }
+    
+    int pop() {
+
+    }
+    
+    int peek() {
+
+    }
+    
+    bool empty() {
+
+    }
+};
+
+// 20E
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto& ch:s){
+            st.push(ch);
+            if()
+        }
+
+    }
+};
+
+
+int main(){
+    return 0;
+}
